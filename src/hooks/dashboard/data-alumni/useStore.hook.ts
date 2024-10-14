76a2a2f914281c6480cea3a3e93b87f4205189b3
geralditheo <geralditheo@gmail.com>
2024-10-14 12:30:45 +0700

    Improve constant.

--- src/hooks/dashboard/data-alumni/useStore.hook.ts
+++ src/hooks/dashboard/data-alumni/useStore.hook.ts
@@ -5,7 +5,8 @@ interface Data {
     name?: string,
     isFilled?: boolean,
     count?: number,
-    lastUpdted?: Date | null
+    lastUpdted?: Date | null,
+    path?: string
 }
 
 
@@ -24,13 +25,13 @@ export const useCheckDataAlumni = create<State & Action>((set) => ({
     // * States
     level: 4,
     data: [
-        { key: 'a1', name: "Academic", isFilled: true, count: 5, lastUpdted:  new Date('10/7/2024') },
-        { key: 'a2', name: "Pekerjaan", isFilled: false, count: 0, lastUpdted: null },
-        { key: 'a3', name: "Magang", isFilled: true, count: 3, lastUpdted:  new Date('10/7/2024')},
-        { key: 'a4', name: "Organization", isFilled: false, count: 0, lastUpdted: null },
-        { key: 'a5', name: "Achievement", isFilled: false, count: 0, lastUpdted: null },
-        { key: 'a6', name: "Certificate", isFilled: true , count: 3, lastUpdted:  new Date('10/7/2024')},
-        { key: 'a7', name: "Skills", isFilled: true, count: 1 , lastUpdted:  new Date('10/7/2024') },
+        { key: 'a1', name: "Academic", isFilled: true, count: 5, lastUpdted:  new Date('10/7/2024'), path: "academic" },
+        { key: 'a2', name: "Job", isFilled: false, count: 0, lastUpdted: null, path: "job" },
+        { key: 'a3', name: "Internship", isFilled: true, count: 3, lastUpdted:  new Date('10/7/2024'), path: "internship"},
+        { key: 'a4', name: "Organization", isFilled: false, count: 0, lastUpdted: null, path: "organization" },
+        { key: 'a5', name: "Achievement", isFilled: false, count: 0, lastUpdted: null, path: "award" },
+        { key: 'a6', name: "Certificate", isFilled: true , count: 3, lastUpdted:  new Date('10/7/2024'), path: "course"},
+        { key: 'a7', name: "Skills", isFilled: true, count: 1 , lastUpdted:  new Date('10/7/2024'), path: "skill" },
     ],
 
     // * Actions

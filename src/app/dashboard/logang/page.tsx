364cd9ae0a167c260e593185ac53cb09b916b103
geralditheo <geralditheo@gmail.com>
2024-10-15 11:34:33 +0700

    Add internship page.

---
+++ src/app/dashboard/logang/page.tsx
@@ -0,0 +1,12 @@
+import LogangIndex from "@/components/logang/LogangIndex"
+
+
+
+export default function Logang(){
+    return <main>
+        <h2 className="text-2xl font-semibold text-gray-500 mb-3 text-center sm:text-left" >Lowongan Magang (Internship)</h2>
+
+        <LogangIndex />        
+
+    </main>
+}

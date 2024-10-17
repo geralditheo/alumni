7c224c59284d7c3f361f2bdd5f432f598711d461
geralditheo <geralditheo@gmail.com>
2024-10-17 15:06:58 +0700

    Improve desc.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Sign Alumni",
+  description: "Page for sign Alumni."
 };
 
 export default function AuthLayout({ children, }: Readonly<{ children: React.ReactNode;}>) {

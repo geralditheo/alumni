ef925746004f6aa46d669f49e4c9c44ecaba3e5e
geralditheo <geralditheo@gmail.com>
2024-10-24 18:57:37 +0700

    Add forgot password hooks.

--- src/hooks/auth/forgotPassword.ts
+++ src/hooks/auth/forgotPassword.ts
@@ -1,36 +1,74 @@
 import axios from "axios";
 import { useState } from "react";
-import { toast } from 'sonner';
+
+type ForgotPassword = {
+    email?: string;
+    message?: string;
+}
 
 export const forgotPassword = () => {
 
-    const [otp, setOtp] = useState<string>('123456');
-    const [verified, setVeried] = useState<boolean>(true);
+    const [isOtpSend, setIsOtpSend] = useState<boolean>(false);
+    const [isVerified, setIsVerifed] = useState<boolean>(true);
 
-    const sendOtp  = async (formData: FormData): Promise< void | undefined > => {
+    const sendOtp  = async (formData: FormData): Promise< void | undefined | unknown> => {
+
         try {
-            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/forgot-password`, formData);
-
-            console.log(data);
             
-            toast.success("Success to send email!");
+            const { data } = await axios.post<ForgotPassword>(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/forgot-password`, formData);
+
+            setIsOtpSend(true);
+            
+            return data;
             
         } catch (error) {
+
             console.log("Error", error);
-            toast.error("Failed to send email!");
+
+            throw new Error();
         }
     
     }
 
-    const verifyOtp  = async (formData: FormData): Promise< void | undefined > => {
+    const verifyOtp  = async (formData: FormData): Promise< void | undefined | unknown> => {
+
+        try {
+            
+            const { data } = await axios.post<ForgotPassword>(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/verify-otp-password`, formData);
+
+            setIsVerifed(true);
+            
+            return data;
+
+        } catch (error) {
+
+            console.log("Error", error);
+
+            throw new Error();
+
+        }
     
     }
 
-    const resetPassword  = async (formData: FormData): Promise< void | undefined > => {
+    const resetPassword  = async (formData: FormData): Promise< void | undefined | unknown> => {
+
+        try {
+            
+            const { data } = await axios.post<ForgotPassword>(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/reset-password`, formData);
+
+            return data;
+
+        } catch (error) {
+
+            console.log("Error", error);
+
+            throw new Error();
+
+        }
     
     }
 
-    return { sendOtp, verifyOtp, resetPassword, otp, verified };
+    return { sendOtp, verifyOtp, resetPassword, isOtpSend, isVerified };
 }
 
 

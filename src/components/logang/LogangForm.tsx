8ee7d8c0353046e0fa6339a45038900c25c21cac
geralditheo <geralditheo@gmail.com>
2024-10-29 19:32:47 +0700

    Improve logang form. Connect API.

--- src/components/logang/LogangForm.tsx
+++ src/components/logang/LogangForm.tsx
@@ -5,6 +5,10 @@ import { useEffect } from "react";
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { getPengalamanMagang } from '@/constant/internship/pengalamanMagang';
 import { getTipeMagang } from '@/constant/internship/tipeMagang';
+import { getUser } from '@/hooks/auth/authClient';
+import { toast } from 'sonner';
+import { useLogang } from '@/hooks/logang/useStore.hook';
+import moment from "moment";
 
 type Inputs = {
     agencyName: string;
@@ -12,26 +16,48 @@ type Inputs = {
     address: string;
     email: string;
     intershipExperience: string;
-    wages: number;
+    wages: number | null;
     intershipType: string;
     description: string;
     url: string;
     tags: string;
     isPublic: boolean;
-    logoFile: File;
+    logoFile: FileList;
+    validPeriod: Date | null | string;
 };
 
 
 export default function LogangForm({ show, hide, uuid }: { show?: boolean , hide?: () => void, uuid?: string | null }){
+    const user = getUser();
 
     const { register, handleSubmit, reset, setValue } = useForm<Inputs>();
     const { data: optionsInternExp } = getPengalamanMagang();
     const { data: optionsInternType } = getTipeMagang();
+    const { post: postLogang, show: showLogang, update: updateLogang } = useLogang();
+
 
     const onSubmit: SubmitHandler<Inputs> =  async (data) => {
+        const file = data.logoFile?.[0] ?? null;
+        const formData = uuid ? new URLSearchParams() : new FormData();
 
-        console.log("Data", data);
-        
+        if (user?.id) formData.append('user_id', user.id);
+        if (data.agencyName) formData.append('NamaPerusahaan', data.agencyName);
+        if (data.position) formData.append('Posisi', data.position);
+        if (data.address) formData.append('Alamat', data.address);
+        if (data.intershipExperience) formData.append('Pengalaman', data.intershipExperience);
+        if (data.wages) formData.append('Gaji', String(data.wages));
+        if (data.intershipType) formData.append('TipeMagang', data.intershipType);
+        if (data.description) formData.append('Deskripsi', data.description);
+        if (data.url) formData.append('Website', data.url);
+        if (data.email) formData.append('Email', data.email);
+        if (data.tags) formData.append('Tags', data.tags);
+        if (data.isPublic) formData.append('Verify', String(data.isPublic));
+        if (formData instanceof FormData) if (file) formData.append('Logo', file);
+        if (data.validPeriod) formData.append('MasaBerlaku', moment(data.validPeriod).format('YYYY-MM-DD'));
+
+        if (!uuid) await postLogang(formData);
+
+        if (uuid) await updateLogang(uuid, formData);
         
         reset();
         if (hide) hide();
@@ -40,14 +66,28 @@ export default function LogangForm({ show, hide, uuid }: { show?: boolean , hide
     useEffect(() => {
 
         if (uuid) {
-            
-            setValue("agencyName", "Kampus Udinus");
-            setValue("position", "Informatics");
-            setValue("intershipExperience", "Tanpa Pengalaman");
-            setValue("wages", 3000000);
-            setValue("isPublic", true);
-          
-            
+            showLogang(uuid)
+                .then((result) => {
+                    const isPulicated = result?.Verify === "true" ? true : result?.Verify === "false" ? false : false;
+                    const newDate = result?.MasaBerlaku ? moment(result?.MasaBerlaku).format("YYYY-MM-DD") : null;
+
+                    setValue("agencyName", result?.NamaPerusahaan ? result?.NamaPerusahaan : "" );
+                    setValue("position", result?.Posisi ? result?.Posisi : "" );
+                    setValue("address", result?.Alamat ? result?.Alamat : "" );
+                    setValue("email", result?.Email ? result?.Email : "" );
+                    setValue("intershipExperience", result?.Pengalaman ? result?.Pengalaman : "");
+                    setValue("wages", result?.Gaji ? Number(result?.Gaji)  : null);
+                    setValue("intershipType", result?.TipeMagang ? result?.TipeMagang : "");
+                    setValue("description", result?.Deskripsi ? result?.Deskripsi : "");
+                    setValue("url", result?.Website ? result?.Website : "");
+                    setValue("tags", result?.Tags ? result?.Tags : "");
+                    setValue("isPublic", isPulicated);
+                    setValue("validPeriod", newDate );
+
+                })
+                .catch(() => {
+                    toast.error("Failed to show!");
+                });
         }
 
     }, [uuid])
@@ -91,7 +131,7 @@ export default function LogangForm({ show, hide, uuid }: { show?: boolean , hide
 
                 <div className="flex flex-col gap-1 mb-5">
                     <label htmlFor="wages" className="text-sm sm:text-base" >Gaji</label>
-                    <input  {...register('wages')} name="wages" id="wages" type="number" min={0} className="text-sm" placeholder="Semarang" />
+                    <input  {...register('wages')} name="wages" id="wages" type="number" min={0} className="text-sm" placeholder="3000000" />
                 </div>
 
                 <div className="flex flex-col gap-1 mb-5">
@@ -130,6 +170,11 @@ export default function LogangForm({ show, hide, uuid }: { show?: boolean , hide
                 <div className="flex flex-col gap-1 mb-5">
                     <label htmlFor="logoFile" className="text-sm sm:text-base" >Logo Perusahaan</label>
                     <input  {...register('logoFile')} name="logoFile" id="logoFile" type="file" className="text-sm"  />
+                </div>
+
+                <div className="flex flex-col gap-1 mb-5">
+                    <label htmlFor="validPeriod" className="text-sm sm:text-base" >Masa Berlaku</label>
+                    <input  {...register('validPeriod', { valueAsDate: true })} name="validPeriod" id="validPeriod" type="date" className="text-sm" />
                 </div>
 
                 <button type="submit" className="bg-blue-500 px-5 py-1 rounded-md text-white w-full sm:w-auto ">Submit</button>

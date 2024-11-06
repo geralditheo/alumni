a9820c0c76331ac932b8e3609eac8271bba7bacd
geralditheo <geralditheo@gmail.com>
2024-11-06 19:06:36 +0700

    Change based on role on dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,20 +4,26 @@ import DashboardLoker from "@/components/dashboard/lowongan-kerja/DashboardLoker
 import DashboardLogang from "@/components/dashboard/lowongan-magang/DashboardLogang";
 import DashboardStatistikAlumni from '@/components/dashboard/statistik-alumni/DashboardStatistikAlumni';
 
-export default function Dashboard(){
-    return <main className="container mx-auto lg:px-32" >
+import { getUserServer } from '@/hooks/auth/authServer';
 
-        <h2 className="font-semibold text-gray-500 text-2xl mb-3" >Welcome, Username</h2>
+export default async function Dashboard(){
+
+    const user = await getUserServer();
+
+    const roleAlumni: boolean | undefined = user?.roles?.includes('alumni');
+    const roleAdmin: boolean | undefined = user?.roles?.includes('admin');
+    const roleMahasiswa: boolean | undefined = user?.roles?.includes('mahasiswa');
+
+    return <main className="container mx-auto lg:px-32 " >
+
+        <h2 className="font-semibold text-gray-500 text-2xl mb-3" >Welcome, {user?.name ?? "Username" }</h2>
 
         <div className="p-3  rounded-md bg-blue-500 h-[200px] mb-3 flex items-center justify-center" ><p className='font-semibold text-5xl text-center text-white' >Portal Alumni</p>  </div>
 
-        <DashboardStatistikAlumni />
-        
-        <DashboardAlumni />
-
-        <DashboardLogang />
-
-        <DashboardLoker />
+        { ( roleAdmin ) && <DashboardStatistikAlumni /> }
+        { ( roleAlumni || roleMahasiswa ) && <DashboardAlumni /> }
+        { ( roleAlumni || roleMahasiswa ) && <DashboardLogang /> }
+        { ( roleAlumni || roleMahasiswa ) && <DashboardLoker /> }
 
     </main>
 }

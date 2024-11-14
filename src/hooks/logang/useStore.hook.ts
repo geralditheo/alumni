b7be987e0836b02687c2dfc1ec18eb95e58e1c0b
geralditheo <geralditheo@gmail.com>
2024-11-14 20:16:22 +0700

    Add API Logang Admin.

--- src/hooks/logang/useStore.hook.ts
+++ src/hooks/logang/useStore.hook.ts
@@ -22,7 +22,7 @@ type Logang = {
     updated_at: string;
 }
 
-export function useLogang(){
+export function useLogangAlumni(){
     const token = getToken();
     const [data, setData] = useState<Logang[]>([]);
     const [manageData, setManageDta] = useState<Logang[]>([]);
@@ -135,6 +135,138 @@ export function useLogang(){
         try {
 
             const { data } = await axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logang/${uuid}`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            return data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error delete logang");
+        }
+
+    }
+
+    return { data, manageData, index, show, post, manage, update, remove };
+}
+
+export function useLogangAdmin(){
+    const token = getToken();
+    const [data, setData] = useState<Logang[]>([]);
+    const [manageData, setManageDta] = useState<Logang[]>([]);
+
+    const manage = async (): Promise< Logang[] | void> => {
+        try {
+
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/manageLogangAdmin` , {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (Array.isArray(data.data)) setManageDta(data.data);
+
+            return data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error manage logang");
+        }
+
+    }
+
+    const index = async (): Promise< Logang[] | void> => {
+        try {
+
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logangAdmin` , {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (Array.isArray(data.data)) setData(data.data);
+
+            return data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error index logang");
+        }
+
+    }
+
+    const show = async (uuid: string): Promise<Logang | undefined> => {
+        try {
+
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logangAdmin/${uuid}` , {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            return data.data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error show logang");
+        }
+
+    }
+
+    const post = async (formData : FormData | URLSearchParams): Promise<void> => {
+        try {
+
+            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logangAdmin`, formData , {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            return data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error post logang");
+        }
+
+    }
+
+    const update = async (uuid: string, formData : FormData | URLSearchParams): Promise<void> => {
+        try {
+
+            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logangAdmin/${uuid}`, formData , {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            return data;
+            
+        } catch (error) {
+
+            console.log("Error", error);
+            
+            throw new Error("Error update logang");
+        }
+
+    }
+
+    const remove = async (uuid: string): Promise<void> => {
+        try {
+
+            const { data } = await axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/logangAdmin/${uuid}`, {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }

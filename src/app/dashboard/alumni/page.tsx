d72d2b76f2662beb445daac4c997bbc055539f8a
geralditheo <geralditheo@gmail.com>
2024-11-18 19:38:18 +0700

    Add CV Alumni. Component

--- src/app/dashboard/alumni/page.tsx
+++ src/app/dashboard/alumni/page.tsx
@@ -1,9 +1,11 @@
 import DataAlumniPage from "@/components/alumni/DataAlumniPage"
-
+import CvAlumni from '@/components/cv-alumni/cvAlumni';
 
 
 export default function Alumni(){
     return <section>
         <DataAlumniPage />
+        <CvAlumni /> 
+        <div></div>
     </section>
 }
---
+++ src/components/cv-alumni/cvAlumni.tsx
@@ -0,0 +1,7 @@
+
+
+export default function CvAlumni(){
+    return <section>
+        Hello, linkediT
+    </section>
+}

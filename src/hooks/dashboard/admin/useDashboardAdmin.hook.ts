f0d6bd1d4c2020627f0318127f5646068d5d274e
geralditheo <geralditheo@gmail.com>
2024-11-22 15:35:28 +0700

    Add hooks for dashboard admin.

---
+++ src/hooks/dashboard/admin/useDashboardAdmin.hook.ts
@@ -0,0 +1,91 @@
+'use client';
+
+import axios from "axios";
+import { getToken } from '@/hooks/auth/authClient';
+import { useState } from "react";
+
+type DashboardAlumni = {
+    bekerja: number;
+    bidangJob: DABidangJob;
+    detectedAlumni3Years: string;
+    lingkupJob: DALingkupJob;
+    skills: DASkill;
+    statistiks: DAStatistik;
+    statusCounts: DAStatusCount;
+    tidakBekerja: number;
+    totalAlumni3Years: number;
+    totalAlumniAllYears: number;
+}
+
+type DABidangJob = {
+    infokomCount: number;
+    nonInfokomCount: number;
+}
+
+type DALingkupJob = {
+    internasional: number;
+    lokalBerbadanHukum: number;
+    lokalTidakBerbadanHukum: number;
+    multinasional: number;
+    nasional: number;
+}
+
+type DASkill = {
+    ahli_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    etoskerja_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    inggris_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    kepemimpinan_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    kerjasama_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    komunikasi_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+    pengembangan_skill: { "Sangat Baik" : 0, "Baik" :0, "Cukup" : 0, "Kurang" : 0 },
+}
+
+type DAStatistik = {
+    data: DAStatistikData[];
+}
+
+type DAStatistikData = {
+    id: number;
+    tahun_lulus: number;
+    alumni_total: number;
+    alumni_terlacak: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
+type DAStatusCount = {
+    ["Bekerja Full Time"]: number;
+    ["Bekerja Part Time"]: number;
+    ["Belum Memungkinkan Bekerja"]: number;
+    ["Melanjutkan Pendidikan"]: number;
+    ["Menikah/Mengurus Keluarga"]: number;
+    ["Tidak Bekerja Tetapi Sedang Mencari Pekerjaan"]: number;
+    ["Wirausaha"]: number;
+}
+
+export function useDashboardAdmin(){
+
+    const [ data, setData ]= useState<DashboardAlumni>();
+
+    const getDashboardAdmin = async () => {
+        const token = getToken();
+
+        try {
+
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/dashboardAdmin`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (data) setData(data);
+
+            
+        } catch (error) {
+            console.log("Error", error);
+            throw new Error("Error get data");
+        }
+    }
+
+    return { data, getDashboardAdmin }
+}

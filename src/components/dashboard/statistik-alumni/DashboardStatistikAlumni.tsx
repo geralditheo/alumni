3d16a6f88083262773902e914a7b0cee670a4ca5
geralditheo <geralditheo@gmail.com>
2024-11-22 15:36:03 +0700

    Update Dashbord admin.

--- src/components/dashboard/statistik-alumni/DashboardStatistikAlumni.tsx
+++ src/components/dashboard/statistik-alumni/DashboardStatistikAlumni.tsx
@@ -1,49 +1,97 @@
 'use client';
 
 import dynamic from "next/dynamic";
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 const PieChart = dynamic(() => import('@/components/chart/PieChart'), { ssr: false });
 const DoughnutChart = dynamic(() => import('@/components/chart/DoughnoutChart'), { ssr: false });
 const LineChart = dynamic(() => import('@/components/chart/LineChart'), { ssr: false });
 
+import { useDashboardAdmin } from '@/hooks/dashboard/admin/useDashboardAdmin.hook';
+import { DataDiagram } from '@/components/chart/DoughnoutChart';
+
 export default function DsahboardStatistikAlumni(){
 
-    return <section>
+    const { data, getDashboardAdmin } = useDashboardAdmin();
+    const [ dataStatusCount, setDataStatusCount ] = useState<DataDiagram[]>([]);
+    const [ dataLinkupJob, setDataLingkupJob ] = useState<DataDiagram[]>([]);
 
-        <div className="flex flex-row flex-wrap gap-y-3 justify-center " >
+    useEffect(() => {
+        getDashboardAdmin()
+    }, [])
 
-            <div className="basis-4/12 p-3 overflow-x-auto " >
-                <Suspense fallback={<div>Loading...</div>} >
-                    <PieChart />
-                </Suspense>
-            </div>
+    useEffect(() => {
+        if (data?.statusCounts){
+            const temp = [];
+            for (const [key, value] of Object.entries(data.statusCounts)) {
+                temp.push({ label: key, data: value });
+            }
+            setDataStatusCount(temp);
+        }
+    }, [data?.statusCounts])
 
-            <div className="basis-4/12 p-3 overflow-x-auto " >
-                <Suspense fallback={<div>Loading...</div>} >
-                    <DoughnutChart />
-                </Suspense>
-            </div>
+    useEffect(() => {
+        if (data?.lingkupJob) {
+            const temp = [];
+            for (const [key, value] of Object.entries(data.lingkupJob)) {
+                temp.push({ label: key, data: value });
+            }
+            setDataLingkupJob(temp);
+        }
+    } ,[data?.lingkupJob])
 
-            <div className="basis-4/12 p-3 overflow-x-auto items-center flex " >
-                <Suspense fallback={<div>Loading...</div>} >
-                    <LineChart />
-                </Suspense>
-            </div>
+    console.log("Data", data);
+    
 
-            <div className="basis-4/12 p-3 overflow-x-auto " >
-                <Suspense fallback={<div>Loading...</div>} >
-                    <PieChart />
-                </Suspense>
-            </div>
+    return (
+        <div>
 
-            <div className="basis-4/12 p-3 overflow-x-auto " >
-                <Suspense fallback={<div>Loading...</div>} >
-                    <DoughnutChart />
-                </Suspense>
-            </div>
-            
+            <section></section>
+
+            <main>
+
+                <div className="flex gap-3 sm:flex-row flex-col mb-3" >
+                    <div className="text-xs sm:text-base basis-full flex gap-3 flex-col" >
+
+                        <div className=" basis-full flex gap-3" >
+                            <div className="basis-full bg-blue-500 shadow rounded-md p-3 " >
+                                <h3 className="font-semibold text-white mb-1" >Alumni 3TH</h3>
+                                <div className=" mb-3" />
+                                <p className="text-white" >Jumlah Alumni Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.totalAlumni3Years}</span> </p>
+                            </div>
+
+                            <div className="basis-full bg-cyan-500 shadow rounded-md p-3 " >
+                                <h3 className="font-semibold text-white mb-1" >Alumni Terdeteksi</h3>
+                                <div className=" mb-3" />
+                                <p className="text-white" >Jumlah Alumni yang Terdeteksi Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.detectedAlumni3Years}</span> </p>
+                            </div>
+                        </div>
+
+                        <div className="basis-full bg-sky-500 shadow rounded-md p-3 " >
+                            <h3 className="font-semibold text-white mb-1" >Jumlah Keseluruhan Alumni</h3>
+                            <div className=" mb-3" />
+                            <p className="text-white" >Jumlah Keseluruhan Alumni dari Berbagai Tahun: <span className="font-semibold text-yellow-300 text-xl " >{data?.totalAlumniAllYears}</span> </p>
+                        </div>
+                    </div>
+
+                    <div className="basis-full h-full sm:h-64 flex justify-center  grow-0 " >
+                        <Suspense fallback={<div>Loading...</div>} >
+                            <DoughnutChart title="Status Users" lable="Counted" dataDiagram={dataStatusCount} isShowLable   />
+                        </Suspense>
+                    </div>
+                </div>
+
+                <div className="flex gap-3 sm:flex-row flex-col" >
+                    <div className="basis-full flex justify-center  grow-0" >
+                        <Suspense fallback={<div>Loading...</div>} >
+                            <PieChart key={"dlj"} dataDiagram={dataLinkupJob} lable=" #" title="Lingkup Pekerjaan" isShowLable />
+                        </Suspense>
+                    </div>
+
+                    <div className="basis-full" >A</div>
+                </div>
+
+            </main>
         </div>
-        
-    </section>
+    ) 
 }

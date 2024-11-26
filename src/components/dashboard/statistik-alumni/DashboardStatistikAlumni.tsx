8a928933a7246d576f8b103d435fc3d235fc24d0
geralditheo <geralditheo@gmail.com>
2024-11-26 19:06:13 +0700

    Improve koor dashboard.

--- src/components/dashboard/statistik-alumni/DashboardStatistikAlumni.tsx
+++ src/components/dashboard/statistik-alumni/DashboardStatistikAlumni.tsx
@@ -3,7 +3,6 @@
 import dynamic from "next/dynamic";
 import { Suspense, useEffect, useState } from 'react';
 
-const PieChart = dynamic(() => import('@/components/chart/chartjs/PieChart'), { ssr: false });
 const DoughnutChart = dynamic(() => import('@/components/chart/chartjs/DoughnoutChart'), { ssr: false });
 const LineChart = dynamic(() => import('@/components/chart/chartjs/LineChart'), { ssr: false });
 
@@ -15,6 +14,9 @@ export default function DsahboardStatistikAlumni(){
     const { data, getDashboardAdmin } = useDashboardAdmin();
     const [ dataStatusCount, setDataStatusCount ] = useState<DataDiagram[]>([]);
     const [ dataLinkupJob, setDataLingkupJob ] = useState<DataDiagram[]>([]);
+    const [ dataKerja, setDataKerja ] = useState<DataDiagram[]>([]);
+    const [ dataBidangKerja, setDataBidangKerja ] = useState<DataDiagram[]>([]);
+    const [ dataStatistik, setDataStatistik ] = useState<DataDiagram[]>([]);
 
     useEffect(() => {
         getDashboardAdmin()
@@ -40,60 +42,84 @@ export default function DsahboardStatistikAlumni(){
         }
     } ,[data?.lingkupJob])
 
-    console.log("Data", data);
-    
+    useEffect(() => {
+        if (data?.bekerja || data?.tidakBekerja){
+            const temp = [];
+            temp.push({ label: 'Bekerja', data: data.bekerja });
+            temp.push({ label: 'Tidak Bekerja', data: data.tidakBekerja });
+            setDataKerja(temp);
+        }
+    }, [data?.bekerja, data?.tidakBekerja])
+
+    useEffect(() => {
+        if (data?.bidangJob) {
+            const temp = [];
+            for (const [key, value] of Object.entries(data.bidangJob)) {
+                temp.push({ label: key, data: value });
+            }
+            setDataBidangKerja(temp);
+        }
+    } ,[data?.bidangJob])
+
+    useEffect(() => {
+        if (data?.statistiks) if (data.statistiks.data) if (data.statistiks.data.length > 0) {
+            const temp = data.statistiks.data.map((item) => ({ label: String(item.tahun_lulus), data: item.alumni_terlacak }))
+            setDataStatistik(temp);
+        }
+    } ,[data?.statistiks])    
 
     return (
-        <div>
-
-            <section></section>
-
-            <main>
-
-                <div className="flex gap-3 sm:flex-row flex-col mb-3" >
-                    <div className="text-xs sm:text-base basis-full flex gap-3 flex-col" >
-
-                        <div className=" basis-full flex gap-3" >
-                            <div className="basis-full bg-blue-500 shadow rounded-md p-3 " >
-                                <h3 className="font-semibold text-white mb-1" >Alumni 3TH</h3>
-                                <div className=" mb-3" />
-                                <p className="text-white" >Jumlah Alumni Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.totalAlumni3Years}</span> </p>
-                            </div>
-
-                            <div className="basis-full bg-cyan-500 shadow rounded-md p-3 " >
-                                <h3 className="font-semibold text-white mb-1" >Alumni Terdeteksi</h3>
-                                <div className=" mb-3" />
-                                <p className="text-white" >Jumlah Alumni yang Terdeteksi Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.detectedAlumni3Years}</span> </p>
-                            </div>
-                        </div>
-
-                        <div className="basis-full bg-sky-500 shadow rounded-md p-3 " >
-                            <h3 className="font-semibold text-white mb-1" >Jumlah Keseluruhan Alumni</h3>
-                            <div className=" mb-3" />
-                            <p className="text-white" >Jumlah Keseluruhan Alumni dari Berbagai Tahun: <span className="font-semibold text-yellow-300 text-xl " >{data?.totalAlumniAllYears}</span> </p>
-                        </div>
-                    </div>
-
-                    <div className="basis-full h-full sm:h-64 flex justify-center  grow-0 " >
-                        <Suspense fallback={<div>Loading...</div>} >
-                            <DoughnutChart title="Status Users" lable="Counted" dataDiagram={dataStatusCount} isShowLable   />
-                        </Suspense>
-                    </div>
+        <main>
+            <div className="flex flex-col sm:flex-row gap-3 mb-3" >
+                <div className="basis-full  bg-blue-500 shadow rounded-md p-3 " >
+                    <h3 className="font-semibold text-white mb-1" >Alumni 3TH</h3>
+                    <div className=" mb-3" />
+                    <p className="text-white" >Jumlah Alumni Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.totalAlumni3Years}</span> </p>
                 </div>
 
-                <div className="flex gap-3 sm:flex-row flex-col" >
-                    <div className="basis-full flex justify-center  grow-0 border" >
-                        <Suspense fallback={<div>Loading...</div>} >
-                            <PieChart key={"dlj"} dataDiagram={dataLinkupJob} lable=" #" title="Lingkup Pekerjaan" isShowLable />
-                        </Suspense>
-                    </div>
-
-                    <div className="basis-full border" >
-                        <LineChart />
-                    </div>
+                <div className="basis-full bg-cyan-500 shadow rounded-md p-3 " >
+                    <h3 className="font-semibold text-white mb-1" >Alumni Terdeteksi</h3>
+                    <div className=" mb-3" />
+                    <p className="text-white" >Jumlah Alumni yang Terdeteksi Dalam 3 Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.detectedAlumni3Years}</span> </p>
                 </div>
 
-            </main>
-        </div>
+                <div className="basis-full bg-sky-500 shadow rounded-md p-3 " >
+                    <h3 className="font-semibold text-white mb-1" >Jumlah Keseluruhan Alumni</h3>
+                    <div className=" mb-3" />
+                    <p className="text-white" >Jumlah Keseluruhan Alumni dari Berbagai Tahun: <span className="font-semibold text-yellow-300 text-xl" >{data?.totalAlumniAllYears}</span> </p>
+                </div>
+            </div>
+
+            <div className="flex gap-3 mb-3 flex-col md:flex-row " >
+                <div className="basis-full flex justify-center h-96 grow-0" >
+                    <Suspense fallback={<div>Loading...</div>} >
+                        <DoughnutChart key={"su"} title="Status Users" lable="Counted" dataDiagram={dataStatusCount} isShowLable   />
+                    </Suspense>
+                </div>
+                <div className="basis-full flex justify-center h-96 grow-0" >
+                    <Suspense fallback={<div>Loading...</div>} >
+                        <DoughnutChart key={"dk"} dataDiagram={dataKerja} lable=" #" title="Kerja" isShowLable />
+                    </Suspense>
+                </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-3 mb-3 " >
+                <div className="basis-full flex justify-center h-96 grow-0" >
+                    <Suspense fallback={<div>Loading...</div>} >
+                        <DoughnutChart key={"dlj"} dataDiagram={dataLinkupJob} lable=" #" title="Lingkup Pekerjaan" isShowLable />
+                    </Suspense>
+                </div>
+                <div className="basis-full flex justify-center h-96 grow-0" >
+                    <Suspense fallback={<div>Loading...</div>} >
+                        <DoughnutChart key={"bj"} title="Bidang Kerja" lable="#" dataDiagram={dataBidangKerja} isShowLable   />
+                    </Suspense>
+                </div>
+            </div>
+
+            <div className="flex justify-center h-96 grow-0" >
+                <LineChart key={"ds"} title="Statistik" lable="Jumlah Alumni" dataDiagram={dataStatistik} isShowLable />
+            </div>
+
+        </main>
     ) 
 }

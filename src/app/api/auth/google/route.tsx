4ddd44cd186d082f5e8ec66a3479fb0e4fd77131
geralditheo <geralditheo@gmail.com>
2024-12-02 10:29:41 +0700

    Add login by sso api.

--- src/app/api/auth/google/route.tsx
+++ src/app/api/auth/google/route.tsx
@@ -1,9 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
-export async function GET(request: NextRequest){
+export function GET(request: NextRequest){
 
-    console.log("R", request);
-    
+    const token = request.nextUrl.searchParams.get("token");
+    const user = request.nextUrl.searchParams.get("user");
 
-    return NextResponse.json({ message: "Google" });
+    if (!token || ! user) return NextResponse.json({ message: "Invalid SSO Callback parameters" }, { status: 400 });
+
+    try {
+        
+        const decodedUser = JSON.parse(Buffer.from(user, 'base64').toString('utf-8'));
+
+        cookies().set("next-token", token, { maxAge: 60 * 60 });
+        cookies().set("next-user", JSON.stringify(decodedUser), { maxAge: 60 * 60 });
+
+        return NextResponse.redirect(new URL("/dashboard"));
+    } catch (error) {
+        console.error('Error handling SSO callback:', error);
+        return NextResponse.json({ error: 'Failed to handle Google SSO callback.' }, { status: 500 });
+    }
 }

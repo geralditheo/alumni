96d886cd82b2eddfb6a4244091bdeb80b27be46f
geralditheo <geralditheo@gmail.com>
2024-12-04 19:07:23 +0700

    Fix error on logang show component.

--- src/components/logang/LogangShow.tsx
+++ src/components/logang/LogangShow.tsx
@@ -18,8 +18,8 @@ export default function LogangShow({ uuid, show, onDone }: {uuid?: string, show?
 
     const setTags = (tags: string) => {
         const split = tags.split(',');
-        return split.map((tag) => {
-            return <span className="px-2 py-1 text-xs font-semibold text-white bg-green-500 rounded-full">{tag}</span>
+        return split.map((tag, index) => {
+            return <span key={index} className="px-2 py-1 text-xs font-semibold text-white bg-green-500 rounded-full">{tag}</span>
         })
     }
 
@@ -64,7 +64,7 @@ export default function LogangShow({ uuid, show, onDone }: {uuid?: string, show?
                 <div className="flex items-center justify-center text-center bg-blue-500 bg-opacity-30 p-3 rounded-lg">
                     <div className="flex flex-col items-center">
                     <img
-                        src={data?.Logo ? `${process.env.NEXT_PUBLIC_API_URL}/storage/imglogo/${data.Logo}` : '/default-logo.png'}
+                        src={data?.Logo ? `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/storage/imglogo/${data.Logo}` : `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/storage/imglogo/default-logo`}
                         alt={`${data?.NamaPerusahaan || 'Perusahaan'} Logo`}
                         className="h-24 w-24 object-contain"
                     />

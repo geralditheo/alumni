b3488f24afa621d0e5adb28f8fc74876dfb30367
geralditheo <geralditheo@gmail.com>
2024-12-04 18:33:32 +0700

    Improve hook alumni dashboard.

--- src/hooks/dashboard/alumni/useStore.hook.ts
+++ src/hooks/dashboard/alumni/useStore.hook.ts
@@ -3,35 +3,58 @@
 import axios from "axios";
 import { getToken } from '@/hooks/auth/authClient';
 import { useState } from "react";
+import { Logang } from '@/hooks/logang/useStore.hook';
+import { Loker } from '@/hooks/loker/useStore.hook';
 
+type TypeDashboardAlumni = {
+    academic: TypePoint
+    award: TypePoint
+    course: TypePoint
+    internship: TypePoint
+    organization: TypePoint
+    skill: TypePoint
+    work: TypePoint   
+}
 
-type dashboardAlumni = {
-    data: {
-        dataStatus: {},
-        logangs: [],
-        lokers: [],
-    }
+type TypePoint = {
+    count: number
+    exists: boolean
+    updated_at: Date
 }
 
 export function useDashboardAlumni(){
 
     const token = getToken();
 
-    const [dataDashboardAlumni, setDataDashboardAlumni] = useState<dashboardAlumni>();
+    const [tCount, setTCount] = useState<number>(0)
+    const [dataDashboardAlumni, setDataDashboardAlumni] = useState<TypeDashboardAlumni>();
+    const [dataDashboardLogang, setDataDashboardLogang] = useState<Logang[]>([]);
+    const [dataDashboardLoker, setDataDashboardLoker] = useState<Loker[]>([]);
     
 
     const getDashboardAlumni = async () => {
         try {
 
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/dashboardAlumni`, {
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/dashboardAlumni-dataAlumni`, {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }
             });
 
-            setDataDashboardAlumni(response.data);
+            if (data.status){
+                setDataDashboardAlumni(data.dataStatus);
+                const statuses = data.dataStatus;
+                const temp = [];
+                for (const key in statuses) {
+                    if (statuses.hasOwnProperty(key)) {
+                        if (statuses[key]?.['count'] > 0) temp.push(key);
+                    }
+                }
+                setTCount(temp.length);
+                return data.dataStatus;
+            }
 
-            return response.data;
+
     
         } catch (error) {
             console.log("Error", error);
@@ -40,8 +63,44 @@ export function useDashboardAlumni(){
 
     }
 
+    const getDashbLogang = async () => {
+        try {
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/dashboardAlumni-logang`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (data.data){
+                setDataDashboardLogang(data.data);
+                return data.data;
+            }
+        } catch (error) {
+            console.log("Error", error);
+            throw new Error("Error create awards");
+        }
+    }
+
+    const getDashbLoker = async () => {
+        try {
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/dashboardAlumni-loker`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (data.data){
+                setDataDashboardLoker(data.data);
+                return data.data;
+            }
+        } catch (error) {
+            console.log("Error", error);
+            throw new Error("Error create awards");
+        }
+    }
+
     
 
-    return { dataDashboardAlumni, getDashboardAlumni }; 
+    return { tCount, dataDashboardAlumni, dataDashboardLogang, dataDashboardLoker, getDashboardAlumni, getDashbLogang, getDashbLoker }; 
 
 }

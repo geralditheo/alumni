ce010ad83ecd45af439b678a3cbff1acda514cc2
geralditheo <geralditheo@gmail.com>
2024-12-05 19:02:54 +0700

    Add container on loker and logang.

--- src/app/dashboard/logang/manage/page.tsx
+++ src/app/dashboard/logang/manage/page.tsx
@@ -3,7 +3,7 @@ import LogangTable from "@/components/logang/LogangTable"
 
 
 export default function Manage(){
-    return <main>
+    return <main className="container mx-auto px-32"  >
         <h2 className="text-2xl font-semibold text-gray-500 mb-3 text-center sm:text-left" >Manage (Internship)</h2>
 
         <LogangTable />
--- src/app/dashboard/loker/manage/page.tsx
+++ src/app/dashboard/loker/manage/page.tsx
@@ -1,7 +1,7 @@
 import LokerTable from "@/components/loker/LokerTable"
 
 export default function Manage(){
-    return <main>
+    return <main className="container mx-auto px-32" >
         <h2 className="text-2xl font-semibold text-gray-500 mb-3 text-center sm:text-left" >Manage</h2>
 
         <LokerTable />
